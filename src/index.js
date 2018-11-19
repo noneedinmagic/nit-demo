14526a38dbec4d7ff2ada4687b045a5736a41206
Oleg <noneedinmagic@gmail.com>
2018-11-19 11:16:52 +0200

    Add jQuery via Webpack

--- src/index.js
+++ src/index.js
@@ -11,3 +11,21 @@ _foo('user2');
 
 // import 'bootstrap';	// with JS!!
 // import 'bootstrap/dist/css/bootstrap.min.css';	// only minified CSS
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+jQuery.ajax({
+	url: 'https://nit.tron.net.ua/api/product/list',
+	method: 'get',
+	dataType: 'json',
+	success: function(json){
+		console.log('Loaded via AJAX!');
+		// console.log(json);
+		console.table(json);
+	},
+	error: function(xhr){
+		alert("An error occured: " + xhr.status + " " + xhr.statusText);
+	},
+});
