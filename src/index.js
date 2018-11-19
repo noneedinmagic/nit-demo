2a32b9208e81c3246a22ba93a42bc76005e774a6
Oleg <noneedinmagic@gmail.com>
2018-11-19 10:41:46 +0200

    Import Bootstrap CSS

--- src/index.js
+++ src/index.js
@@ -8,3 +8,6 @@ require('./modules/hello')('NIT');
 var _foo = require('./modules/hello');
 _foo('user1');
 _foo('user2');
+
+// import 'bootstrap';	// with JS!!
+import 'bootstrap/dist/css/bootstrap.min.css';	// only minified CSS
